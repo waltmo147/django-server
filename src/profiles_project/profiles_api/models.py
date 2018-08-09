e83ff8f7ebe9dc1e8f350dd2e56481665fd51367
Walt Mo <apple@Walt-ModeiMac.local>
2018-08-09 22:10:17 +0800

    add profilenmanager and data migration

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,7 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Helps Django work with our custum user model."""
+
+    def create_user(self, email, name, password=None):
+        """Creates a new user profile object"""
+        if not email:
+            raise ValueError('Users must have an email address.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details."""
+
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
@@ -31,4 +60,3 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """Django uses this to convert the object to string"""
 
         return self.email
-        
