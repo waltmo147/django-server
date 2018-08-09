ba8a51d6b561c58465b68cb57d17359bbcb0c81c
Walt Mo <apple@Walt-ModeiMac.local>
2018-08-09 22:59:52 +0800

    added url patterns that matches our hello-view api

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View."""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features."""
+
+        an_apiview = {
+            'Uses HTTP methods as function as function (get, post, patch, put, delete)',
+            'It is similar to a traditional Django view',
+            'Gives you the mose control over your logic',
+            'Its mapped manually to URLS'
+        }
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
