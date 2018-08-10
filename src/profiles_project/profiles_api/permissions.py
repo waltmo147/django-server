1c38b97bdea40e8299de5bea6aa6bfe189db408c
Walt Mo <apple@Walt-ModeiMac.local>
2018-08-10 17:11:31 +0800

    delete read only

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -12,3 +12,14 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return False
+
+class PostOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status."""
+
+    def has_object_permission(self, request, view, obj):
+        """checks the user is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if obj.user_profile.id == request.user.id:
+            return True
+        return False
--- src/profiles_project/profiles_api/serializers.py
+++ src/profiles_project/profiles_api/serializers.py
@@ -12,7 +12,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.UserProfile
-        fields = ['id', 'email', 'name', 'password']
+        fields = ('id', 'email', 'name', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
@@ -26,3 +26,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """A serializer for profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
