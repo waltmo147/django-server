7ee86fa8d72b26d1abf0fc4977363c4916f09b19
Walt Mo <apple@Walt-ModeiMac.local>
2018-08-10 17:42:07 +0800

    added deployment setting again

--- deploy/server_setup.sh
+++ deploy/server_setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/LondonAppDeveloper/byob-profiles-rest-api.git'
+PROJECT_GIT_URL='https://github.com/waltmo147/django-server.git'
 
 PROJECT_BASE_PATH='/usr/local/apps'
 VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
