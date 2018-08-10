6dec5978fec26dc257bcb47c2d47ae54e17ea451
Walt Mo <apple@Walt-ModeiMac.local>
2018-08-10 14:38:10 +0800

    added login and tested

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -6,9 +6,10 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
+router.register('login', views.LoginViewSet, base_name='login')
+
 
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
