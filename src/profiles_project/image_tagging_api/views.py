b011c21bcf641f38f4718f2e56152c80e1daa10e
Walt Mo <apple@Walt-ModeiMac.local>
2018-11-15 20:16:14 -0500

    self

--- src/profiles_project/image_tagging_api/views.py
+++ src/profiles_project/image_tagging_api/views.py
@@ -28,42 +28,41 @@ import sys
 class UploadImageViewSet(viewsets.ViewSet):
     """Test API ViewSet."""
 
+    self.IMAGE_DIR = '/home/ubuntu/data/ImageNet_mini/'
+    self.PROJECT_DIR = '/usr/local/apps/profiles-rest-api'
     #serializer_class = serializers.HelloSerializer
 
     def list(self, request):
         """Return a hello message"""
-        IMAGE_DIR = '/home/ubuntu/data/ImageNet_mini/'
-        PROJECT_DIR = '/usr/local/apps/profiles-rest-api'
+
         tag_id = self.request.query_params.get('tag_id')
         num = int(self.request.query_params.get('num'))
         count = int(self.request.query_params.get('count'))
         width = int(self.request.query_params.get('width'))
 
-
-
         # no such category in server
-        if not os.path.exists(IMAGE_DIR + tag_id):
+        if not os.path.exists(self.IMAGE_DIR + tag_id):
             return Response({'message': 'no such id', 'image': ''})
 
         if count == 1:
-            ls = os.listdir(IMAGE_DIR + tag_id)
+            ls = os.listdir(self.IMAGE_DIR + tag_id)
             # discard '.DS_Store' file
             if num >= len(ls):
                 return Response({'message': 'fail', 'image': ''})
-            path = IMAGE_DIR + tag_id + '/' +ls[num]
+            path = self.IMAGE_DIR + tag_id + '/' +ls[num]
             with open(path, 'rb') as imageFile:
                 image_str = base64.b64encode(imageFile.read())
             return Response({'message': 'success', 'image': image_str})
 
         if count > 1:
-            ls = os.listdir(IMAGE_DIR + tag_id)
+            ls = os.listdir(self.IMAGE_DIR + tag_id)
             image_arr = []
             has_more = True
             for i in range(num, num + count):
                 if i >= len(ls):
                     has_more = False
                     break
-                path = IMAGE_DIR + tag_id + '/' +ls[i]
+                path = self.IMAGE_DIR + tag_id + '/' +ls[i]
 
                 img = Image.open('path')
                 w, h = img.size
@@ -85,10 +84,10 @@ class UploadImageViewSet(viewsets.ViewSet):
         """upload an image to recognize"""
 
         #serializer = serializers.HelloSerializer(data=request.data)
-        fh = open(PROJECT_DIR + "/imageToSave.JPEG", "wb")
+        fh = open(self.PROJECT_DIR + "/imageToSave.JPEG", "wb")
         encoded = base64.b64decode(request.data['image'])
         fh.write(encoded)
-        cmd = 'PYTHONPATH=' + PROJECT_DIR + '/android-model403/ python -m scripts.label_image  --image=' + PROJECT_DIR + '/imageToSave.jpeg'
+        cmd = 'PYTHONPATH=' + self.PROJECT_DIR + '/android-model403/ python -m scripts.label_image  --image=' + self.PROJECT_DIR + '/imageToSave.jpeg'
         result = subprocess.check_output(cmd, shell=True)
 
         string = result.decode(encoding='UTF-8')
